fcdaca86fb94a7509febf5b1120066ffca93d094
Rian Novais <riannovais3@gmail.com>
2023-12-09 11:53:48 -0300

    adding a function that performs DDL on the database, (creating tables and relations)

--- database.py
+++ database.py
@@ -7,3 +7,45 @@ class Database():
         self.conn = sqlite3.Connection(self.path)
         self.cursor = self.conn.cursor()
 
+        self.create_tables()
+
+    #DDL: Creating tables of database
+    def create_tables(self):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS regions("
+                            "id INTEGER NOT NULL,"
+                            "acronym TEXT(2) NOT NULL,"
+                            "name TEXT NOT NULL,"
+                            "CONSTRAINT regions_PK PRIMARY KEY (id))")
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS states("
+                            "id INTEGER NOT NULL,"
+                            "acronym TEXT(2) NOT NULL,"
+                            "name TEXT NOT NULL,"
+                            "idRegion INTEGER NOT NULL,"
+                            "CONSTRAINT states_PK PRIMARY KEY (id),"
+                            "CONSTRAINT states_FK FOREIGN KEY (idRegion) REFERENCES regions(id)"
+                            "ON DELETE CASCADE ON UPDATE CASCADE)")
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS cities("
+                            "id INTEGER NOT NULL,"
+                            "name TEXT NOT NULL,"
+                            "stateId INTEGER NOT NULL, "
+                            "CONSTRAINT cities_PK PRIMARY KEY (id),"
+                            "CONSTRAINT cities_FK FOREIGN KEY (stateId) REFERENCES states(id)"
+                            "ON DELETE CASCADE ON UPDATE CASCADE)")
+
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS districts("
+                            "id INTEGER NOT NULL,"
+                            "name TEXT NOT NULL,"
+                            "cityId INTEGER NOT NULL,"
+                            "cityName TEXT NOT NULL,"
+                            "CONSTRAINT districts_PK PRIMARY KEY (id),"
+                            "CONSTRAINT districts_FK FOREIGN KEY (cityId) REFERENCES cities(id)"
+                            "ON DELETE CASCADE ON UPDATE CASCADE)")
+
+        self.conn.commit()
+
+
+
+if __name__ == "__main__":
+    db = Database()
