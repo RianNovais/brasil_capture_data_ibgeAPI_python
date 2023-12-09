dc772248b21e4a344d0f72d12d3abd9d43b05d63
Rian Novais <riannovais3@gmail.com>
2023-12-09 12:36:58 -0300

    adding function that extracts regions from the api and inserts them into the database

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import sqlite3
 from pathlib import Path
+import requests
 
 class Database():
     def __init__(self):
@@ -7,7 +8,10 @@ class Database():
         self.conn = sqlite3.Connection(self.path)
         self.cursor = self.conn.cursor()
 
+        self.regionsUrl = "https://servicodados.ibge.gov.br/api/v1/localidades/regioes"
+
         self.create_tables()
+        self.add_regions_to_db()
 
     #DDL: Creating tables of database
     def create_tables(self):
@@ -45,6 +49,30 @@ class Database():
 
         self.conn.commit()
 
+    def add_regions_to_db(self):
+        #verificando se ja existem dados na tabela, pra nao dar erro
+        self.cursor.execute('SELECT * FROM regions')
+        if len(self.cursor.fetchall()) == 0:
+            #try/except pra caso dê erro na requisição
+            try:
+                responseRegions = requests.get(self.regionsUrl).json()
+            except:
+                print(f'ERROR IN GET -> {self.regionsUrl}')
+                return
+            #pra cada regiao no JSON que veio da api, ele vai pegar cada campo e depois adicionar na tabela
+            for region in responseRegions:
+                _id = region['id']
+                acronym = region['sigla']
+                name = region['nome']
+
+                data = [_id, acronym, name]
+                self.cursor.execute('INSERT INTO regions (id, acronym, name) VALUES'
+                                '(?, ?, ?)', data)
+
+                self.conn.commit()
+            print('REGIONS ADDED SUCESSFULLY TO DATABASE')
+            return
+        print('ERROR: REGIONS TABLE IS NOT EMPTY')
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import requests
+from database import Database
+
 
 if __name__ == "__main__":
-    ...
-# response = requests.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados').json()
-# for estado in response:
-#
-#     print(estado['id'], estado['sigla'], estado['nome'])
+    db = Database()
